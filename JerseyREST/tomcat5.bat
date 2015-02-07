590a5620026eba07dedf1c4226c69173c4d0bb7b
Justin01Wu <justin01.wu@gmail.com>
2015-02-07 17:41:01 -0500

    deploy batch file for tomcat 5.5

---
+++ JerseyREST/tomcat5.bat
@@ -0,0 +1,29 @@
+@echo off
+
+rem call maven to build
+call mvn clean package
+
+rem shutdown tomcat
+SET CATALINA_HOME=C:\program2\apache-tomcat-5.5.26
+call C:\program2\apache-tomcat-5.5.26\bin\shutdown.bat
+
+rem wait for seconds
+ping 1.1.1.1 -n 1 -w 3000 > nul
+
+rem delete deploy target folder
+echo.
+echo deleting folder C:\program2\apache-tomcat-5.5.26\webapps\jersey-rest
+rmdir /S /Q C:\program2\apache-tomcat-5.5.26\webapps\jersey-rest
+echo deleting folder C:\program2\apache-tomcat-5.5.26\work\Catalina
+rmdir /S /Q C:\program2\apache-tomcat-5.5.26\work\Catalina
+
+rem copy war file
+echo copying file build\results_dnf\jersey-rest.war
+copy /Y target\jersey-rest.war C:\program2\apache-tomcat-5.5.26\webapps
+
+rem start tomcat
+cd C:\program2\apache-tomcat-5.5.26\bin
+call _debug.bat
+cd C:\projects\WebApp\WebApp\JerseyREST
+
+rem pause
