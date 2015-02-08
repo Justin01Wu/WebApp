d9831865c319dcf9cd0cdaec8701ec4fc23006e6
Justin01Wu <justin01.wu@gmail.com>
2015-02-07 19:19:56 -0500

    add first java json mapping api, still can run in tomcat 5.5

--- JerseyREST/src/main/java/wu/justin/rest/UserService.java
+++ JerseyREST/src/main/java/wu/justin/rest/UserService.java
@@ -5,6 +5,8 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import wu.justin.rest.dto.User;
+
 //it comes from 
 // http://www.vogella.com/tutorials/REST/article.html
 
@@ -16,6 +18,14 @@ public class UserService {
 	@Path("/json")
 	public String sayJsonHello() {
 	    return "{\"user\":\"Justin Wu\"}";
+	}
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/current")
+	public User getCurrentUser() {
+		User user = new User(12347, "Justin Wu");
+	    return user;
 	}
 	
 }
---
+++ JerseyREST/src/main/java/wu/justin/rest/dto/User.java
@@ -0,0 +1,32 @@
+package wu.justin.rest.dto;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class User {
+	
+	private Integer id;	
+	private String name;
+	
+	public User(){		
+	}
+	public User(Integer id, String name){
+		this.id = id;
+		this.name = name;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+}
