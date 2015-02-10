e477a3d96254420ea0901a82e31896d642a242e0
Justin Wu <justin01.wu@gmail.com>
2015-02-10 15:41:25 -0500

    Using Variables in Windows Batch Files

--- JerseyREST/deploy.bat
+++ JerseyREST/deploy.bat
@@ -1,27 +1,28 @@
 call mvn clean package
-rem call mvn clean package
+
+rem set tomcat home
+SET CATALINA_HOME=C:\program2\tomcat-7.0.55-x64
 
 rem shutdown tomcat
-SET CATALINA_HOME=C:\program2\apache-tomcat-7.0.55
-call C:\program2\apache-tomcat-7.0.55\bin\shutdown.bat
+call %CATALINA_HOME%\bin\shutdown.bat
 
 rem wait for seconds
 ping 1.1.1.1 -n 1 -w 3000 > nul
 
 rem delete deploy target folder
 echo.
-echo deleting folder C:\program2\apache-tomcat-7.0.55\webapps\jersey-rest
-rmdir /S /Q C:\program2\apache-tomcat-7.0.55\webapps\jersey-rest
-echo deleting folder C:\program2\apache-tomcat-7.0.55\work\Catalina
-rmdir /S /Q C:\program2\apache-tomcat-7.0.55\work\Catalina
+echo deleting folder %CATALINA_HOME%\webapps\jersey-rest
+rmdir /S /Q %CATALINA_HOME%\webapps\jersey-rest
+echo deleting folder %CATALINA_HOME%\work\Catalina
+rmdir /S /Q %CATALINA_HOME%\work\Catalina
 
 rem copy war file
 echo copying file target\jersey-rest.war
-copy /Y target\jersey-rest.war C:\program2\apache-tomcat-7.0.55\webapps
+copy /Y target\jersey-rest.war %CATALINA_HOME%\webapps
 
 rem start tomcat
-cd C:\program2\apache-tomcat-7.0.55\bin
+cd %CATALINA_HOME%\bin
 call _debug.bat
-cd C:\projects\WebApp\WebApp\JerseyREST
+cd C:\samples\WebApp\WebApp\JerseyREST
 
 rem pause
