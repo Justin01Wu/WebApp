465d3cc4291ed566442020b148867d82ac756c4c
Justin01Wu <justin01.wu@gmail.com>
2015-02-14 18:32:55 -0500

    improve UI experience for Demo

---
+++ JerseyJson/src/main/webapp/index.js
@@ -0,0 +1,40 @@
+
+if (!window.console) {
+	window.console = {
+		log : function() {
+		}
+	};
+}
+
+function getAPIUrlBase(){
+	return "api";
+	
+	// to avoid CORS trouble and test it local, please add this on the Chrome command line :  --disable-web-security
+	//return "http://localhost:8080/api";
+}
+
+function callMenuApi(){
+	var menuUrl = getAPIUrlBase() + '/user/current/menu';
+	//var menuUrl = 'WEB-INF/menus_uw.json';  // for local testing
+	//var menuUrl = 'WEB-INF/menus_analysis2.json';  // for local testing
+	//var menuUrl = 'WEB-INF/menus_admin.json';  // for local testing
+	jQuery("#urlStr").text(menuUrl);
+	jQuery.ajax({
+		url : menuUrl,
+		dataType : 'json',
+		success : function(json) {
+			console.log("got menu json...");
+			jQuery("#responseJson").text(JSON.stringify(json, null, 4));
+			
+			
+		},
+		error: function(jqXHR,error, errorThrown) {  
+			console.error("loading menu error with status: " + jqXHR.status);
+            if(jqXHR.status&&jqXHR.status==400){
+                 alert(jqXHR.responseText); 
+            }else{
+                alert("loading menu failed, please refresh page or contact admin");
+            }
+       }
+	});
+}
