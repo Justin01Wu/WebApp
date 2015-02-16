a0909afa7d3a5f4fa3ae2c6bebbb44db050d028d
Justin01Wu <justin01.wu@gmail.com>
2015-02-16 10:35:36 -0500

    add comment for @JsonProperty

--- JerseyJson/src/main/java/wu/justin/rest/dto/UserDTO.java
+++ JerseyJson/src/main/java/wu/justin/rest/dto/UserDTO.java
@@ -5,6 +5,8 @@ import org.codehaus.jackson.annotate.JsonProperty;
 
 import wu.justin.business.User;
 
+// For more details, please see
+// http://www.cowtowncoder.com/blog/archives/2011/02/entry_443.html
 public class UserDTO  extends User {
 	
 	public UserDTO(Integer id, String name){
