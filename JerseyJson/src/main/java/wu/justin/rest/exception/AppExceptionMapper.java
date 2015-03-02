92ffbbaf40b27671318ed2dc51fb545d09e85d2a
Justin01Wu <justin01.wu@gmail.com>
2015-03-01 21:24:29 -0500

    add comment

--- JerseyJson/src/main/java/wu/justin/rest/exception/AppExceptionMapper.java
+++ JerseyJson/src/main/java/wu/justin/rest/exception/AppExceptionMapper.java
@@ -6,6 +6,7 @@ import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
 // comes from http://www.codingpedia.org/ama/error-handling-in-rest-api-with-jersey/
+// and http://www.codingpedia.org/ama/custom-reason-phrase-in-http-status-error-message-response-with-jax-rs-jersey/
 
 @Provider
 public class AppExceptionMapper implements ExceptionMapper<AppException> {
