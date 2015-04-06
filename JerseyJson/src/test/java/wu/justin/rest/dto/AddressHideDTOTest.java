fd074b2c3017fa41a3780f17038e41346602a6bd
Justin Wu <justin01.wu@gmail.com>
2015-04-06 10:54:40 -0400

    try to check field is ignored in jackson

--- JerseyJson/src/test/java/wu/justin/rest/dto/AddressHideDTOTest.java
+++ JerseyJson/src/test/java/wu/justin/rest/dto/AddressHideDTOTest.java
@@ -1,12 +1,17 @@
 package wu.justin.rest.dto;
 
+import static org.junit.Assert.assertTrue;
+
 import java.io.IOException;
 
 import org.codehaus.jackson.JsonGenerationException;
 import org.codehaus.jackson.map.JsonMappingException;
 import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.map.introspect.AnnotatedClass;
+import org.codehaus.jackson.map.introspect.AnnotatedField;
+import org.codehaus.jackson.map.introspect.AnnotatedMethod;
+import org.codehaus.jackson.xc.JaxbAnnotationIntrospector;
 import org.junit.Test;
-import static org.junit.Assert.assertTrue;
 
 // it comes from 
 // http://www.mkyong.com/java/how-to-convert-java-object-to-from-json-jackson/
@@ -28,6 +33,46 @@ public class AddressHideDTOTest {
 		assertTrue(result.getId() == 0);
 		assertTrue(result.getCountry() == null);
 		assertTrue(result.getAddress().equals("ShenZhen box 2345"));
+	}
+	
+	@Test
+	public void testFields() {
+		
+		JaxbAnnotationIntrospector  ai = new JaxbAnnotationIntrospector ();
+		AnnotatedClass ac = AnnotatedClass.construct(AddressHideDTO.class, ai, null);
+		ac.resolveFields();
+		ac.resolveClassAnnotations();
+		ac.resolveMemberMethods(null);
+		ac.resolveClassAnnotations();
+		System.out.println("fieldCount=" + ac.getFieldCount());
+		
+		for(AnnotatedField f: ac.fields()){
+			if(ai.isIgnorableField(f)){
+				System.out.println("ignored: " + f.getName());	
+			}else{
+				System.out.println("valid: " + f.getName());
+			}			
+		}
+		// always return "valid", don't know why
+		
+		System.out.println("");
+		
+		for(AnnotatedMethod m: ac.memberMethods()){
+			if(ai.isIgnorableMethod(m)){
+				System.out.println("ignored: " + m.getName());	
+			}else{
+				System.out.println("valid: " + m.getName());
+			}			
+		}
+		// always return "valid", don't know why		
+		
+		
+		String[] ignoredList = ai.findPropertiesToIgnore(ac);		
+		// ignoredList is always null, don't know why
+//		for(String one: ignoredList){
+//			System.out.println(one);
+//		}
+
 	}
 
 }
