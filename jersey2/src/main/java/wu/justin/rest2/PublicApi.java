d41067f6fd9c55b18726a7c5f35075eead03e19c
Justin Wu <justin01.wu@gmail.com>
2016-03-25 15:58:40 -0400

    fix partial json return issue

--- jersey2/src/main/java/wu/justin/rest2/PublicApi.java
+++ jersey2/src/main/java/wu/justin/rest2/PublicApi.java
@@ -1,14 +1,21 @@
 package wu.justin.rest2;
 
+import java.io.IOException;
 import java.util.Date;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import wu.justin.bean.Address;
 import wu.justin.bean.User;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectReader;
 
 @Path("/public")
 public class PublicApi {
@@ -36,6 +43,36 @@ public class PublicApi {
 		
 		System.out.println("getCurrentUser...");
 		
+		User user = createUser();
+		return user;
+	}
+	
+	/** demo how to convert java object tree to json 
+	 * @throws IOException */
+	@PUT
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Path("/user")
+	public User setCurrentUser(User userDTO) throws IOException {
+		
+		System.out.println("setCurrentUser...");
+		
+		User currentUser = createUser();
+		
+		
+		// use userDTO to override currentUser to avoid partial json return because @JsonIgnore will ignore some data
+		// for detail, please see http://stackoverflow.com/questions/12518618/deserialize-json-into-existing-object-java
+		// or http://stackoverflow.com/questions/9895041/merging-two-json-documents-using-jackson
+		ObjectMapper objectMapper = new ObjectMapper();
+		ObjectReader updater = objectMapper.readerForUpdating(currentUser);
+		String overridesJson = objectMapper.writeValueAsString(userDTO);		
+		User merged = updater.readValue(overridesJson);
+		
+		
+	    return merged;
+	}
+	
+	private static User createUser(){
 		User user = new User(56239, "Justin Wu");
 		user.addEmails("justin01.wu@gmail.com");
 		user.addEmails("wuyg719@gmail.com");
@@ -52,6 +89,7 @@ public class PublicApi {
 		
 		user.setDesc("This is a sample to demo how Jackson convert a Java object tree into json");
 	    return user;
+		
 	}
 
 }
