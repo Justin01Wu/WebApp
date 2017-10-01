507fa873dc8d1e17d9ca90796fff5f29b67c8bdc
Justin01Wu <justin01.wu@gmail.com>
2017-10-01 13:54:26 -0400

    improve doclet sample

--- jersey2/src/main/java/wu/justin/rest2/MyDoclet.java
+++ jersey2/src/main/java/wu/justin/rest2/MyDoclet.java
@@ -9,15 +9,54 @@ import com.sun.javadoc.ClassDoc;
 import com.sun.javadoc.RootDoc;
 
 /**
-this is a doclet, please use this command to run it:
+this is a doclet 
 
-javadoc -doclet ca.justa.doclet.MyDoclet -docletpath .\target\justin-doclet.jar C:\samples\JavaFrame\JavaFrame\scjp\src\main\java\scjp\testclass\inherit\HughJampton.java
-javadoc -doclet ca.justa.doclet.MyDoclet -docletpath .\target\justin-doclet.jar C:\samples\WebApp\WebApp\jersey2\src\main\java\wu\justin\rest2\PublicApi.java
 */
 public class MyDoclet {
 	
 	public static boolean start(RootDoc root){
 		
+		printClassPath();
+        
+        for( ClassDoc aClass : root.classes() ){
+        	handleOneClass(aClass);
+        }
+        return true;
+    
+	}
+	
+	private static void handleOneClass(ClassDoc aClass){
+    	AnnotationDesc[] annotations = aClass.annotations();
+    	
+    	boolean isRESTfulAPI = false;
+    	for(AnnotationDesc desc: annotations){
+    		System.out.println("annotation= "+desc);
+    		AnnotationTypeDoc aDoc = desc.annotationType();
+    		String myType = aDoc.toString();
+    		if("javax.ws.rs.Path".equals(myType)){
+    			isRESTfulAPI = true;
+    			break;
+    		}
+    		//System.out.println("aDoc= " + aDoc);
+    		
+    		//System.out.println("aDoc class= " + aDoc.getClass().getName());
+    	}
+    	if(!isRESTfulAPI){
+    		return;
+    	}
+    	System.out.println("  qualifiedName = "  + aClass.qualifiedName());
+    	System.out.println("  name = " + aClass.name());
+    	System.out.println("  commentText = " + aClass.commentText());
+    	System.out.println("  rawCommentText = " + aClass.getRawCommentText());
+    	System.out.println("  dimension = " + aClass.dimension());
+    	System.out.println("  qualifiedTypeName = "+aClass.qualifiedTypeName());
+    	System.out.println("  simpleTypeName = " + aClass.simpleTypeName());
+    	System.out.println("=============  end ======================================================   ");
+    	System.out.println("");
+		
+	}
+	
+	private static void printClassPath(){
 		ClassLoader cl = ClassLoader.getSystemClassLoader();
 
 		URL[] urls = ((URLClassLoader) cl).getURLs();
@@ -26,39 +65,18 @@ public class MyDoclet {
 			System.out.println(url.getFile());
 		}
 		
-        
-        for( ClassDoc aClass : root.classes() ){
-        	AnnotationDesc[] annotations = aClass.annotations();
-        	
-        	boolean isRESTfulAPI = false;
-        	for(AnnotationDesc desc: annotations){
-        		//System.out.println("annotation= "+desc);
-        		AnnotationTypeDoc aDoc = desc.annotationType();
-        		String myType = aDoc.toString();
-        		if("javax.ws.rs.Path".equals(myType)){
-        			isRESTfulAPI = true;
-        			break;
-        		}
-        		//System.out.println("aDoc= " + aDoc);
-        		
-        		//System.out.println("aDoc class= " + aDoc.getClass().getName());
-        	}
-        	if(!isRESTfulAPI){
-        		continue;
-        	}
-        	System.out.println("     =======    ");
+	}
+	
+	public static void main(String[] args) {
 
-        	System.out.println("  qualifiedName = "  + aClass.qualifiedName());
-        	System.out.println("  name = " + aClass.name());
-        	System.out.println("  commentText = " + aClass.commentText());
-        	System.out.println("  rawCommentText = " + aClass.getRawCommentText());
-        	System.out.println("  dimension = " + aClass.dimension());
-        	System.out.println("  qualifiedTypeName = "+aClass.qualifiedTypeName());
-        	System.out.println("  simpleTypeName = " + aClass.simpleTypeName());
+		// String sourcePath = "C:/samples/WebApp/WebApp/jersey2/src/main/java/";
+		String sourcePath = "C:/projects/WebApp/WebApp/jersey2/src/main/java/";
+		String packageName1 = "wu.justin.rest2";
+		String packageName2 = "wu.justin.rest2.user";
+		String[] myArgs = { "-doclet", "wu.justin.rest2.MyDoclet",
+				"-sourcepath", sourcePath, packageName1, packageName2 };
 
-        }
-        return true;
-    
+		com.sun.tools.javadoc.Main.execute(myArgs);
 	}
 
 }
