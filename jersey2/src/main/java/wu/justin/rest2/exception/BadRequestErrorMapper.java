80a74430ef2cffc2cbac15d64a802aa0a70bb3fa
Justin Wu <justin01.wu@gmail.com>
2017-10-24 15:22:10 -0400

    fix a warning

--- jersey2/src/main/java/wu/justin/rest2/exception/BadRequestErrorMapper.java
+++ jersey2/src/main/java/wu/justin/rest2/exception/BadRequestErrorMapper.java
@@ -1,7 +1,5 @@
 package wu.justin.rest2.exception;
 
-import java.util.logging.Logger;
-
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
