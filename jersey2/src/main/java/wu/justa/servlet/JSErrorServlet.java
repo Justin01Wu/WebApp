8a1ec86d61ca768d73fa1a0cfe06f53805e64a14
Justin.Wu <justin.wu@validusholdings.com>
2019-02-12 17:08:28 -0500

    improve JS server log sample

--- jersey2/src/main/java/wu/justa/servlet/JSErrorServlet.java
+++ jersey2/src/main/java/wu/justa/servlet/JSErrorServlet.java
@@ -1,8 +1,5 @@
 package wu.justa.servlet;
 
-import java.io.IOException;
-
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
