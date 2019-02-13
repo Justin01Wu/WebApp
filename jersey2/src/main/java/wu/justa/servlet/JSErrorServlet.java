fd2b3ade13bf40a33d6e7f7eef82859a9711b824
Justin.Wu <justin.wu@validusholdings.com>
2019-02-13 15:40:05 -0500

    improve JS error handling

--- jersey2/src/main/java/wu/justa/servlet/JSErrorServlet.java
+++ jersey2/src/main/java/wu/justa/servlet/JSErrorServlet.java
@@ -1,28 +1,33 @@
 package wu.justa.servlet;
 
+import java.util.logging.Logger;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+ 
 
 public class JSErrorServlet extends HttpServlet {
 	
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 15464563L;
+	
+	private final static Logger log =  Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
 
 	public void doPost(HttpServletRequest request, HttpServletResponse response) {
 
 		String msg = request.getParameter("msg");
 		if (msg !=null && msg.length()>0) {
-			System.out.println(msg);
+			log.warning(msg);
 		}
 		
 		String url = request.getParameter("url");
 		if (url !=null && url.length()>0) {
-			System.out.println(url);
+			log.warning(url);
 		}
 		
 		String line = request.getParameter("line");
 		if (line !=null && line.length()>0) {
-			System.out.println(line);
+			log.warning(line);
 		}
 
     }		
