530fbeed327c8152ed05e2743a49cbaaa7f48b7c
Justin.Wu <justin.wu@validusholdings.com>
2020-09-30 13:53:48 -0400

    improve spnego sample

--- spnego/README.md
+++ spnego/README.md
@@ -12,6 +12,13 @@ Spnego authenticate center
 	+ dozens of social network authenticate
 	+ session status monitor
 	+ realm, client, roles, groups are unnecessary
+	+ try to authorize user, which is incorrect. Every application has different style to manage permission. 
 + also its code quality is not so good, like HttpFailure didn't override getMessage 
 + it is open source project, no vendor is supporting it
 + Also services are using its public key to verify token, which forces services stay inside enterprise network, which should go to cloud
+
+## new project benefits
++ It is so simple, just 3 java classes, less than 100 code line
++ It is too simple to make a bug
++ It give you JWT token directly, which KryCloak didn't
++ Won't sync status after the token is created, so no performance issue
--- spnego/src/main/java/wu/justa/servlet/SecurityServlet.java
+++ spnego/src/main/java/wu/justa/servlet/SecurityServlet.java
@@ -1,22 +1,19 @@
 package wu.justa.servlet;
 
 import java.io.IOException;
-import java.security.Principal;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 
 import wu.justa.model.TokenUser;
 
 /**
- * This Servlet is the authentication and authorization for the application
+ * This Servlet is the authentication user, find who you are on Spnego protocol
  */
 public class SecurityServlet extends HttpServlet {
 
@@ -24,8 +21,7 @@ public class SecurityServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 23423L;
 
-	public static final String LOGIN_ERROR = "/jsp/common/errorLogin.jsp";
-	public static final String PAGE_HOME = "/spnego/index.html";
+	public static final String PAGE_HOME = "/spnego/";
 	public static final String JWT_TOKEN_QUERY = "jwtToken";
 
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
@@ -43,79 +39,60 @@ public class SecurityServlet extends HttpServlet {
 
 		String authDomainUserName = request.getRemoteUser();  // SpnegoHttpFilter set it 
 		
-		Principal p = request.getUserPrincipal(); // SpnegoHttpFilter set it
-		
-		log.info(p);
-		
-		// Spnego set it for us
+//		Principal p = request.getUserPrincipal(); // SpnegoHttpFilter set it		
+//		log.trace(p);
 		
 		HttpSession session = request.getSession();
 
 		if (authDomainUserName == null || authDomainUserName.isEmpty()) {
-
 			log.error(authDomainUserName + "\t" + request.getRemoteAddr() + " \tFailed login attempt");
+			response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
 			return;
-		}
-		;
+		}		
 
 		log.info(authDomainUserName + "\t" + request.getRemoteAddr() + " \t" + "try to login");
 
 		String authUserName = null;
+		TokenUser authUser = new TokenUser();
+		authUser.setUserName(authDomainUserName);
 
+		if (log.isTraceEnabled()) {
+			log.trace("set session attribute of 'user' with " + authUser);
+		}
+		session.setAttribute(ClickstreamFilter.session_user, authUser);
+		
 		try {
-
-			TokenUser authUser = new TokenUser();
-			authUser.setUserName(authDomainUserName);
-
-			if (log.isTraceEnabled()) {
-				log.trace("set session attribute of 'user' with " + authUser);
-			}
-
-			// Loading privilege information here
-			session.setAttribute(ClickstreamFilter.session_user, authUser);
-
-			String redirectUrl = null;
-			String origUrl = request.getParameter(ClickstreamFilter.ORIGINAL_REQUEST_URL);
-			if (origUrl != null) {
-
-				// redirectUrl = URLDecoder.decode(origUrl, "UTF-8");
-				String token = JWTTokenServlet.createToken2(authUser);
-				if(origUrl.contains("?")) {
-					redirectUrl = origUrl + "&"+ JWT_TOKEN_QUERY + "=" + token ;	
-				}else {
-					redirectUrl = origUrl + "?"+ JWT_TOKEN_QUERY + "=" + token ;
-				}				 
-				
-				if (log.isDebugEnabled()) {
-					log.debug("redirect to: " + redirectUrl);
-				}
-			} else {
-				redirectUrl = PAGE_HOME;
-			}
+			String redirectUrl = getRedirectUrl(request, authUser);
 			response.sendRedirect(redirectUrl);
-
 			log.info(authDomainUserName + "\t" + request.getRemoteAddr() + " \t" + "login success");
-
 		} catch (Exception se) {
-			log.log(Level.FATAL, authUserName + "\t" + request.getRemoteAddr() + " \tValidate User Login", se);
 			log.error(authUserName + "\t" + request.getRemoteAddr() + " \tFailed login attempt");
-			dispatch(request, response, LOGIN_ERROR);
+			response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
 		}
 	}
+	
+	private static String getRedirectUrl(HttpServletRequest request, TokenUser authUser) {
+		String redirectUrl = null;
+		String origUrl = request.getParameter(ClickstreamFilter.ORIGINAL_REQUEST_URL);
+		if (origUrl != null) {
 
-	/**
-	 * Handling session attributes and redirections.
-	 * 
-	 * @param request  the HttpServletRequest
-	 * @param response the HttpServletResponse
-	 * @param nextpage the nextpage
-	 * 
-	 * @throws ServletException the servlet exception
-	 * @throws IOException      Signals that an I/O exception has occurred.
-	 */
-	public void dispatch(HttpServletRequest request, HttpServletResponse response, String nextpage)
-			throws ServletException, IOException {
-		RequestDispatcher disp = getServletContext().getRequestDispatcher(nextpage);
-		disp.forward(request, response);
+			String token = JWTTokenServlet.createToken2(authUser);
+			if(origUrl.contains("?")) {
+				redirectUrl = origUrl + "&"+ JWT_TOKEN_QUERY + "=" + token ;	
+			}else {
+				redirectUrl = origUrl + "?"+ JWT_TOKEN_QUERY + "=" + token ;
+			}				 
+			
+			if (log.isDebugEnabled()) {
+				log.debug("redirect to: " + redirectUrl);
+			}
+		} else {
+			redirectUrl = PAGE_HOME;
+			//throw new RuntimeException("Justin test2345");
+		}
+		return redirectUrl;
+
 	}
+
+
 }
