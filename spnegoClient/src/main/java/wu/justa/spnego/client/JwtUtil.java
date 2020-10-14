a53f2a7a75c77300d0ecc1fc5eb1d0fce6041eb1
Justin.Wu <justin.wu@validusholdings.com>
2020-10-14 10:43:41 -0400

    simplified Spnego samples

--- spnegoClient/src/main/java/wu/justa/spnego/client/JwtUtil.java
+++ spnegoClient/src/main/java/wu/justa/spnego/client/JwtUtil.java
@@ -1,24 +1,19 @@
 package wu.justa.spnego.client;
 
 import java.io.IOException;
-import java.util.Base64;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.auth0.jwt.interfaces.JWTVerifier;
 import com.auth0.jwt.interfaces.Verification;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class JwtUtil {
 	public static final String JWT_SECRET = "MyPassword1234";
 
-	private static final String CHARSET_NAME = "UTF-8";
-	
 	public static DecodedJWT verifyToken(String token) throws SecurityException, IOException {
 
-		Algorithm algorithm = extractAlgorithm(token);
+		Algorithm algorithm = extractAlgorithm2(token);
 		Verification ver = JWT.require(algorithm);
 		if(algorithm.getName().equals("HS256")) {
 			ver = ver.withIssuer("Spnego");
@@ -32,33 +27,16 @@ public class JwtUtil {
 
 	}
 
-	private static Algorithm extractAlgorithm(String token) throws SecurityException, IOException {
-		System.out.println("Attempting to extract algorithm information from token.");
-		final String[] tokenParts = token.split("\\.");
-		if (tokenParts.length != 3) { // Header + Body + Signature
-			throw new SecurityException("Invalid token received.");
+	private static Algorithm extractAlgorithm2(String token) throws SecurityException, IOException {
+		JWT parser =  new JWT ();	
+		DecodedJWT jwt = parser.decodeJwt(token);
+		String alg = jwt.getAlgorithm();
+		
+		if (alg.equals("HS256")) {
+			return Algorithm.HMAC256(JWT_SECRET);			
 		}
 
-		String base64EncodedHeader = tokenParts[0];
-		String base64EncodedBody = tokenParts[1];
-
-		String header = new String(Base64.getDecoder().decode(base64EncodedHeader.getBytes()), CHARSET_NAME);
-		String body = new String(Base64.getDecoder().decode(base64EncodedBody.getBytes()), CHARSET_NAME);
-		System.out.println(String.format("Decoded token's header: %s, and boy: %s", header, body));
-
-		// The kid claim indicates the particular public key that was used to sign the token 
-		new ObjectMapper();
-		JsonNode headerNode = new ObjectMapper().readTree(header);
-		JsonNode bodyNode = new ObjectMapper().readTree(body);
-
-		String alg = headerNode.get("alg").textValue();
-		Algorithm a = Algorithm.HMAC256(JWT_SECRET);
-		if (a.getName().equals(alg)) {
-			return a;
-		}else {
-			throw new RuntimeException("unknown Algorithm: " + alg);
-		}
-
+		throw new SecurityException("unknown Algorithm: " + alg );
 	}
-
+	
 }
